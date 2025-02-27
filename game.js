5a05485295e41724903195cd8bf377c3c390f638
PrajapatiWork <work029012@gmail.com>
2025-02-27 16:36:11 +0530

    Added game draw feature code in JS file. Added different color for player. Changed the design for message container

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ let resetBtn = document.querySelector(".resetBtn");
 let newGameBtn = document.querySelector(".newGameBtn");
 let msgContainer = document.querySelector(".msgContainer");
 let msg = document.querySelector("#msg");
-
+let count = 0;
 let playerx = true; // player X and player O 
 const winPaterns = [
     [0, 1, 2],
@@ -17,21 +17,33 @@ const winPaterns = [
 ];
 const resetGame = () => {
     playerx = true;
+    count = 0;
     enableAllBox();
     msgContainer.classList.add("hide");
 };
 
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
+        count++;
 
         if (playerx == true) {
             box.innerText = "X";
+            box.classList.add("playerX");
             playerx = false;
+
         } else {
+            box.classList.remove("playerX");
             box.innerText = "O";
             playerx = true;
         }
         box.disabled = true;
+        console.log(` The count isss ${count}`);
+
+        if (count == 9) {
+            console.log(` The count is 9`);
+            msg.innerText = "No Winner. Play again!"
+            msgContainer.classList.remove("hide");
+        }
         checkWinner();
 
     });
